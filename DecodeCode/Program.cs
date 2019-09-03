ae1e355c2fead8c238b9c1f4786e0feba653e5d6
yangjinyou <770317222@qq.com>
2019-09-03 14:43:45 +0800

    修改

--- DecodeCode/Program.cs
+++ DecodeCode/Program.cs
@@ -11,7 +11,7 @@ namespace DecodeCode
     {
         static void Main(string[] args)
         {
-            string dir = @"G:\miui\qq群722606342";
+            string dir = @"G:\miui";
             if (args.Length > 0)
                 dir = args[0].ToString();
             else
@@ -19,7 +19,7 @@ namespace DecodeCode
                 Console.WriteLine("请输入路径");
                 dir = Console.ReadLine();
             }
-            if (!Directory.Exists(dir))
+            if (!Directory.Exists(dir)&& !File.Exists(dir))
             {
                 Console.WriteLine("路径不存在！");
             }
@@ -29,14 +29,17 @@ namespace DecodeCode
                 if (dirInfo.Root.Name==dirInfo.Name)
                 {
                     Console.WriteLine("不支持根目录！");
-                }else
+                }else if(Directory.Exists(dir))
                 {
-                    Decode(Path.GetFullPath(dir));
+                    DecodeDir(Path.GetFullPath(dir));
+                }else if (File.Exists(dir))
+                {
+                    DecodeFile(Path.GetFullPath(dir));
                 }
             }
             Console.ReadKey();
         }
-        static void Decode(string dirStr)
+        static void DecodeDir(string dirStr)
         {
             string newDir = dirStr + "_new";
             string[] files = Directory.GetFiles(dirStr, "*.*", SearchOption.AllDirectories);
@@ -83,6 +86,37 @@ namespace DecodeCode
             {
                  File.Move(nf,nf.Substring(0,nf.LastIndexOf("_dandy")));
             }
+            Console.WriteLine("处理完成！");
+        }
+
+
+        static void DecodeFile(string pathStr)
+        {
+            string newFile = Path.GetDirectoryName(pathStr)+"\\"+"new_"+ Path.GetFileName(pathStr) + "_dandy"; 
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;    //是否使用操作系统shell启动
+            p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+            p.StartInfo.RedirectStandardOutput = false;//不需要获取输出信息
+            p.StartInfo.RedirectStandardError = true;//不需要重定向标准错误输出,但false时下面的指令未执行
+            p.StartInfo.CreateNoWindow = true;//不显示程序窗口
+            p.Start();//启动程序
+            StreamWriter mySR = p.StandardInput;
+            Console.WriteLine("开始复制处理。。。");
+            mySR.WriteLine($"copy \"{pathStr}\" \"{newFile}\" /y");
+            //str表示一行命令
+            //mySR.WriteLine(str[nowIdx]);
+            //这里的Flush（）操作和我理解的不一样，尽管flush了但是命令行并没有立即执行
+            //而且当需要执行的命令行数量十分多的时候，在使用了WriteLine后会在随机行数之后值执行，可能与缓冲区大小有关
+            mySR.Flush();
+            //这个地方我对于原文所提供的exit和&exit的区别没有完全弄清，以后可能进行补充
+            p.StandardInput.WriteLine("&exit");
+            //关闭StreamWriter后，之前输入的命令行会立即执行
+            mySR.Close();
+            p.WaitForExit();
+            p.Close();
+            Console.WriteLine("开始重命名处理。。。");
+            File.Move(newFile, newFile.Substring(0, newFile.LastIndexOf("_dandy")));
             Console.WriteLine("处理完成！");
         }
     }
